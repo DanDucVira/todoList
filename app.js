8fee2954d56360c155de7a8c5ff43434418453fe
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2019-06-10 13:17:34 -0400

    working on search section

--- app.js
+++ app.js
@@ -32,10 +32,16 @@ list.addEventListener('click', e => {
 });
 
 const filterTodos = (term) => {
-    console.log(term)
-}
+    Array.from(list.children)
+        .filter((todo) => !todo.textContent.toLowerCase().includes(term))
+        .forEach((todo) => todo.classList.add('filtered'));
+
+    Array.from(list.children)
+        .filter((todo) => todo.textContent.toLowerCase().includes(term))
+        .forEach((todo) => todo.classList.add('filtered'));
+};
 //keyup event
 search.addEventListener('keyup', () => {
-    const term = search.value.trim();
-    filterTodos(term)
+    const term = search.value.trim().toLowerCase();
+    filterTodos(term);
 });
