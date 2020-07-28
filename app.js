910e24ec6538f4817207f1a0c23ad9f0f5d836f6
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2020-07-28 01:34:48 -0400

    added animations to project

--- app.js
+++ app.js
@@ -53,7 +53,12 @@ function deleteCheck(e) {
     //Delete todo
     if (item.classList[0] === "delete-btn") {
         const todo = item.parentElement;
-        todo.remove();
+        //animation
+        todo.classList.add("fall");
+        todo.addEventListener("transitionend", function () {
+            todo.remove();
+        });
+
     }
 
 
